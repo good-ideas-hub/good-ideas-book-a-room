f7769b77665dccac6f5de764fda77fb90c2a94e2
cshif <5175166+cshif@users.noreply.github.com>
2024-10-23 16:22:40 +0800

    refactor: drop foreignKey of book_by

---
+++ database/migrations/2024_10_23_080929_drop_book_by_fk_constraint.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign(['book_by']);
+            $table->foreignId('book_by')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign(['book_by']);
+            $table->foreignId('book_by')->constrained('users');
+        });
+    }
+};
