311955eec84d8b179f927e0e1ebfe2738a7fd11e
cshif <5175166+cshif@users.noreply.github.com>
2024-10-24 19:24:12 +0800

    feat: add slack login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/redirect/slack', function () {
+    return Socialite::driver('slack')->redirect();
+});
+
+Route::get('/auth/callback/slack', function () {
+    $slackUser = Socialite::driver('slack')->user();
+
+    $user = User::firstOrCreate([
+        'email' => $slackUser->email,
+    ], [
+        'slack_id' => $slackUser->id,
+        'name' => $slackUser->name,
+        'password' => Hash::make($slackUser->email),
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/admin');
 });
