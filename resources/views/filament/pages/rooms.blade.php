b34bf66cdf67fb5e6e5b5b1bfb3fecaec1c707c8
cshif <5175166+cshif@users.noreply.github.com>
2024-10-26 23:53:14 +0800

    refactor: close #13 navigate to create event page by clicking room

--- resources/views/filament/pages/rooms.blade.php
+++ resources/views/filament/pages/rooms.blade.php
@@ -1,3 +1,80 @@
-<div>
-    <img src="{{env('APP_URL')}}/rooms.png" alt="rooms">
+<div
+  class="grid gap-2"
+  style="
+    grid-template-columns: repeat(13, 58px);
+    grid-template-rows: repeat(24, 8px);
+  "
+>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 3
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 2 / 4; grid-row: 2 / 5; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 3 (中型)</span>
+  </a>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 2
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 4 / 6; grid-row: 2 / 5; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 2 (中型)</span>
+  </a>
+  <div class="flex items-center justify-center" style="grid-column: 8 / 9; grid-row: 1 / 5; background-color: #E9D1DD;">
+    <span class="font-bold text-xs text-center">豆芽房</span>
+  </div>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 4
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 1 / 2; grid-row: 7 / 10; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 4 (小型)</span>
+  </a>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 5
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 1 / 2; grid-row: 10 / 13; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 5 (小型)</span>
+  </a>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 6
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 1 / 2; grid-row: 14 / 19; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 6 (複合式空間)</span>
+  </a>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 7
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 1 / 2; grid-row: 19 / 25; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 7 (複合式空間)</span>
+  </a>
+  <div class="flex items-center justify-center" style="grid-column: 2 / 9; grid-row: 5 / 25; background-color: #FFF3CC;">
+    <span class="font-bold text-xs text-center">進駐區</span>
+  </div>
+  <div class="flex items-center justify-center" style="grid-column: 9 / 13; grid-row: 3 / 25; background-color: #D0E2F3;">
+    <span class="font-bold text-xs text-center">公共區＆學員區</span>
+  </div>
+  <a
+    href="{{ \App\Filament\Resources\EventResource::getUrl('create', [
+      'room_id' => 1
+    ]) }}"
+    class="flex items-center justify-center"
+    style="grid-column: 13 / 14; grid-row: 8 / 15; background-color: #D9EBD3;"
+  >
+    <span class="font-bold text-xs text-center">會議室 1 (大型)</span>
+  </a>
 </div>
