42adaf09b04b1a1ca775dd18809ef963e3b2ee8e
cshif <5175166+cshif@users.noreply.github.com>
2024-10-30 12:51:49 +0800

    fix: fix favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>BookARoom</title>
-        <link rel="icon" href="/favicon.png" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
